47de053afd952f985a2a0e55f7335cdf1f48bcf1
Vitor Vasconcelos <vitors.vasconcelos@gmail.com>
2018-04-02 18:46:29 -0300

    Modificadas as extensoes de dif para diffusion para facilitar no script.

--- profiling/test-profiling.sh
+++ profiling/test-profiling.sh
@@ -3,10 +3,10 @@
 
 for i in tet hex; do
     for j in elements volumes; do
-	for k in s2 dif; do
+	for k in s2 difusion; do
 	    cd cylinder-$i-$j-$k;
 	    milonga ../profiling.mil --$j --$k cylinder-$i-$j-$k > cylinder-$i-$j-$k.dat;
-	    gprof /usr/bin/milonga > cylinder-$i-$j-$k-gprof.txt
+	    gprof /usr/bin/milonga > cylinder-$i-$j-$k-gprof.txt;
 	    cd ..
 	    echo "Done with cylinder-$i-$j-$k."
 	done
