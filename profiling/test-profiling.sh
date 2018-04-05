4d8f189233056ed4eb6e77b25f679c2b9871f9f0
Vitor Vasconcelos <vitors.vasconcelos@gmail.com>
2018-04-05 11:23:24 -0300

    O profiling será feito com o callgrind e não gprof.

--- profiling/test-profiling.sh
+++ profiling/test-profiling.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 # call milonga tests for profiling information
+# This script uses valgrind as profiling tool (callgrind)
 
 for i in tet hex; do
     for j in elements volumes; do
 	for k in s2 diffusion; do
 	    start=`date +%s`
 	    cd cylinder-$i-$j-$k;
-	    milonga ../profiling.mil --$j --$k cylinder-$i-$j-$k;
-	    gprof /usr/bin/milonga > cylinder-$i-$j-$k-gprof.txt;
+	    valgrind --tool=callgrind milonga ../profiling.mil --$j --$k cylinder-$i-$j-$k;
+#	    gprof /usr/bin/milonga > cylinder-$i-$j-$k-gprof.txt;
 	    cd ..
 	    end=`date +%s`
 	    runtime=$((end-start))
